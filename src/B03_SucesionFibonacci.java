2d4b2295cb3ee94da28198438023af6dc1dd9812
Sylvarr <whitessx@gmail.com>
2025-11-15 00:13:39 +0100

    solucion ejercicio fibonacci hasta n

--- src/B03_SucesionFibonacci.java
+++ src/B03_SucesionFibonacci.java
@@ -6,13 +6,12 @@
  * Comienza con 0 y 1: 0, 1, 1, 2, 3, 5, 8, 13, ...
  */
 
-import java.util.Scanner; // Importa la clase Scanner para poder leer la entrada del usuario.
+import java.util.Scanner;
 
 public class B03_SucesionFibonacci {
 
   public static void main(String[] args) {
 
-    // Crea un objeto Scanner para leer la entrada de datos desde la consola.
     Scanner sc = new Scanner(System.in);
 
     System.out.println("Sucesión de Fibonacci");
@@ -25,16 +24,19 @@ public class B03_SucesionFibonacci {
     System.out.println("La sucesion sería: ");
 
     // Inicializa los dos primeros términos de la sucesión de Fibonacci.
-    // t1 representa el término actual y t2 el siguiente en la lógica de actualización.
+    // t1 representa el término actual y t2 el siguiente en la lógica de
+    // actualización.
     int t1 = 0, t2 = 1;
 
     // Bucle para generar y mostrar los 'N' primeros números de la sucesión.
-    // El bucle se ejecuta 'numero' veces, una por cada término que queremos mostrar.
+    // El bucle se ejecuta 'numero' veces, una por cada término que queremos
+    // mostrar.
     for (int i = 1; i <= numero; ++i) {
       // Imprime el término actual (t1).
       System.out.println(t1);
 
-      // Calcula el siguiente término de la sucesión sumando los dos anteriores (t1 y t2).
+      // Calcula el siguiente término de la sucesión sumando los dos anteriores (t1 y
+      // t2).
       int suma = t1 + t2;
 
       // Actualiza los términos para la siguiente iteración:
@@ -45,7 +47,6 @@ public class B03_SucesionFibonacci {
     }
 
     // Cierra el objeto Scanner para liberar los recursos del sistema.
-    // Es una buena práctica cerrar los recursos que se abren.
     sc.close();
 
   }
