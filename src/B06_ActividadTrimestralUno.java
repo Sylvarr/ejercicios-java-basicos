063e7b32cc6daa38ef9af5a4e6066c3456e973a6
Sylvarr <whitessx@gmail.com>
2025-11-19 15:03:38 +0100

    Refactor: Mejora en la actividad trimestral uno
    - Reordenados elementos de salida.
    - Corregido nombre de variable 'textoEnArray'.
    - Implementado contador de caracteres 'numeroDeCaracteres'.

--- src/B06_ActividadTrimestralUno.java
+++ src/B06_ActividadTrimestralUno.java
@@ -17,53 +17,50 @@ public class B06_ActividadTrimestralUno {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+
     System.out.println("Por favor introduce un texto: ");
     String texto = sc.nextLine();
+    String[] textoEnArray = texto.split(" ");
 
     System.out.println();
     System.out.println("RESULTADOS");
     System.out.println("----------");
-    System.out.println();
-    System.out.println("1. La longitud del texto es de " + texto.length() + " caracteres.");
-    System.out.println("2. El texto en mayúsculas seria: " + texto.toUpperCase());
 
-    String[] textoEnArrray = texto.split(" ");
-
-    System.out.println("3. El texto separado por palabras y por fila seria: ");
-    System.out.println();
-
+    int numeroDeCaracteres = 0;
     int letrasMinusculas = 0;
-    for (int i = 0; i < textoEnArrray.length; i++) {
-      System.out.println("--> " + textoEnArrray[i]);
 
-      for (int j = 0; j < textoEnArrray[i].length(); j++) {
-
-        if (textoEnArrray[i].charAt(j) >= 'a' && textoEnArrray[i].charAt(j) <= 'z') {
-          letrasMinusculas++;
-
-        }
-
-      }
-
-    }
-
-    System.out.println("4. El número de letras minúsculas sería " + letrasMinusculas + ".");
+    System.out.println("1. El texto en mayúsculas seria: " + texto.toUpperCase());
+    System.out.println("2. El texto separado por palabras y por fila seria: ");
     System.out.println();
+
+    for (int i = 0; i < textoEnArray.length; i++) {
+      System.out.println("--> " + textoEnArray[i]);
+
+      for (int j = 0; j < textoEnArray[i].length(); j++) {
+
+        numeroDeCaracteres++;
+
+        if (textoEnArray[i].charAt(j) >= 'a' && textoEnArray[i].charAt(j) <= 'z') {
+          letrasMinusculas++;
+        }
+      }
+    }
+    System.out.println();
+    System.out.println("3. La longitud del texto es de " + numeroDeCaracteres + " caracteres.");
+    System.out.println("4. El número de letras minúsculas sería " + letrasMinusculas + ".");
     System.out.println("5. El triángulo con el texto introducido sería: ");
     System.out.println();
 
-    for (int i = textoEnArrray.length; i > 0; i--) {
+    for (int i = textoEnArray.length; i > 0; i--) {
       String fraseCompuesta = "";
 
       for (int j = 0; j < i; j++) {
-        fraseCompuesta += textoEnArrray[j] + " ";
+        fraseCompuesta += textoEnArray[j] + " ";
       }
 
       System.out.println("--> " + fraseCompuesta);
     }
 
     sc.close();
-
   }
-
 }
